fbe99ed9dcc1b98d8c21bccb2b0da4a6b8f7d30c
zuhair <122153573+syedzuhairabbasrizvi@users.noreply.github.com>
2024-06-10 20:53:36 +0500

    Update comment_scraper.js
    added comments

--- comment_scraper.js
+++ comment_scraper.js
@@ -1,4 +1,5 @@
-// Function to extract comments
+
+// get comments of a page snapshot
 function getYouTubeComments() {
     const comments = [];
     document.querySelectorAll('#content-text').forEach(comment => {
@@ -7,7 +8,7 @@ function getYouTubeComments() {
     return comments;
 }
 
-// Scroll to load more comments and collect them
+// scroll down page to get comments
 async function scrollAndCollectComments() {
     let lastHeight = 0;
     let currentHeight = document.documentElement.scrollHeight;
@@ -17,7 +18,7 @@ async function scrollAndCollectComments() {
         lastHeight = currentHeight;
         window.scrollTo(0, currentHeight);
         
-        // Wait for more comments to load
+        // apparently this gives a delay for comments to load before replacing height
         await new Promise(resolve => setTimeout(resolve, 2000));
         
         currentHeight = document.documentElement.scrollHeight;
@@ -28,7 +29,7 @@ async function scrollAndCollectComments() {
     return comments;
 }
 
-// Function to download comments as a UTF-8 encoded file
+// download in UTF-8 because urdu text is involved
 function downloadComments(comments) {
     const blob = new Blob([comments.join('\n')], { type: 'text/plain;charset=utf-8' });
     const url = URL.createObjectURL(blob);
@@ -41,7 +42,6 @@ function downloadComments(comments) {
     URL.revokeObjectURL(url);
 }
 
-// Run the script and save comments to a file
 scrollAndCollectComments().then(comments => {
     console.log('All comments loaded:');
     console.log(comments);
